bd3b7c52c57b59aa1df25e9cfb48012a4410d370
iurybessa <iury.bessa@gmail.com>
2022-04-03 10:43:33 -0300

    minor change

--- optparam.m
+++ optparam.m
@@ -3,7 +3,7 @@
 clc, clear all, close all
 addpath('EEFIG_FULL');
 addpath('data_igbt');
-dv=4;
+dv=3;
 scldfeatname=strcat('device',num2str(dv),'_scaledtrigfeatures.mat')
 featname=strcat('device',num2str(dv),'_features.mat')
 load(scldfeatname); % IGBT Dataset
